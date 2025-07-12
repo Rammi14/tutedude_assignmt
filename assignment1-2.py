485f039bd200170545aa8b82d7b742f40ada2f26
Rammi14 <rcsharma6806@gmail.com>
2025-07-12 22:19:33 +0530

    Add files via upload

---
+++ assignment1-2.py
@@ -0,0 +1,6 @@
+first_name = "Ramesh "
+last_name = "Sharma"
+full_name = first_name + last_name
+print(f"Hello, {full_name}! Welcome to Python world.")
+
+
---
+++ assignment1.py
@@ -0,0 +1,10 @@
+a=input('a: ')
+b=input('b: ')
+a=int(a)
+b=int(b)
+A=a+b
+S=a-b
+M=a*b
+D=a/b
+print(f'Addition={A}, Subtruction={S}, Multiplication={M}, Division={D}')
+
